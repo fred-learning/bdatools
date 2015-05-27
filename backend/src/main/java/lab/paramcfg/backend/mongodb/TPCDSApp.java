6ddb34d80707ecd2468d559f94d81e48f1efdb17
fred-learning <tangyt1990@gmail.com>
2015-05-27 19:05:35 +0800

    fix the error of using availQuery.txt

--- backend/src/main/java/lab/paramcfg/backend/mongodb/TPCDSApp.java
+++ backend/src/main/java/lab/paramcfg/backend/mongodb/TPCDSApp.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Date;
@@ -14,11 +15,11 @@ import lab.paramcfg.backend.common.Util;
 
 public class TPCDSApp {
 	public static void main(String[] args) throws Exception {
-		BufferedReader strCon = new BufferedReader(new FileReader(
-				"src/main/resources/availQuery.txt"));
+		InputStream is = TPCDSApp.class.getResourceAsStream("/availQuery.txt");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(is));
 		String context;
 		ArrayList<String> querylist = new ArrayList<String>();
-		while ((context = strCon.readLine()) != null) {
+		while ((context = reader.readLine()) != null&&!context.equals("")) {
 			querylist.add(context);
 		}
 		int[] numexecutorslist = { 32, 16, 8 };
